986b08165d41fe33b7e2413e9f1abeae65a3fc96
luviguer <luviguera@gmail.com>
2024-10-14 13:56:42 +0200

    esteticaIgual

--- firmadorJavaSpring/project/src/main/java/es/arlabdevelopments/firmador/controller/ApplicationController.java
+++ firmadorJavaSpring/project/src/main/java/es/arlabdevelopments/firmador/controller/ApplicationController.java
@@ -1,6 +1,6 @@
 package es.arlabdevelopments.firmador.controller;
-
 import es.arlabdevelopments.firmador.Libreria;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,43 +28,70 @@ public class ApplicationController {
 
     @GetMapping("/")
     public String handleInit() {
-        return "introduce_fichero";
+        return "index";
     }
 
-    @PostMapping("/upload")
-    public String handleUpload(
-            @RequestParam("archivo") MultipartFile file,
-            Model model) throws IOException {
-        File f = creaFichero(file);
-        model.addAttribute("aliases", Libreria.comprobarAlias(f));
-        model.addAttribute("archivo", Base64.getEncoder().encodeToString(Files.readAllBytes(f.toPath())));
+    
+
+    @GetMapping("/createParticipant")
+    public String handleIni() {
+        return "formularioParticipante";
+    }
+
+    @GetMapping("/intro")
+    public String handleIn() {
         return "peticion_datos";
     }
 
-    @PostMapping("/form")
-    public String handleForm(
-            @RequestParam("seleccion") String alias,
-            @RequestParam("contrasena") String contrasena,
-            @RequestParam("json") String json,
-            @RequestParam("archivo") String file,
-            Model model) throws IOException {
-        File f = creaFichero(file);
-        logger.info("Nombre del fichero: " + f.getName());
-        try {
-            logger.info("Contenido del fichero: " + Base64.getEncoder().encodeToString(Files.readAllBytes(f.toPath())));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        String privateKey = "-----BEGIN PRIVATE KEY-----" +
-                Base64.getEncoder().encodeToString(Libreria.clave(alias, contrasena, f).getEncoded()) + "-----END PRIVATE KEY-----";
 
-        logger.info("Valor de la clave privada: " + privateKey);
-        logger.info("Valor del json: " + json);
+    @PostMapping("/uploadNuevo")
+    public String handleUpload(
+        @RequestParam("archivo") MultipartFile file,
+        @RequestParam("seleccion") String alias,
+        @RequestParam("contrasena") String contrasena,
+        @RequestParam("json") String json,
+        Model model) throws IOException {
 
-        String dev = httpPetition(privateKey, json);
-        model.addAttribute("data", dev);
-        return "muestra_jws";
+    // Verificar que el archivo no esté vacío
+    if (file.isEmpty()) {
+        logger.info("El archivo no se ha subido correctamente.");
+       
+        
     }
+
+
+    // Convierte el MultipartFile a File
+    File f = creaFichero(file);  // Asegúrate que este método funcione correctamente
+
+    // Usa el archivo para comprobar el alias
+    model.addAttribute("aliases", Libreria.comprobarAlias(f));
+
+    // Log del archivo
+    logger.info("Nombre del fichero: " + f.getName());
+    logger.info("Valor del alias: " + alias);
+    logger.info("Valor de la contraseña: " + contrasena);
+    logger.info("Contenido del fichero: " + Base64.getEncoder().encodeToString(Files.readAllBytes(f.toPath())));
+
+    // Generar la clave privada
+    String privateKey = "-----BEGIN PRIVATE KEY-----" +
+            Base64.getEncoder().encodeToString(Libreria.clave(alias, contrasena, f).getEncoded()) + "-----END PRIVATE KEY-----";
+
+    logger.info("Valor de la clave privada: " + privateKey);
+    logger.info("Valor del json: " + json);
+
+    // Aquí puedes realizar la petición HTTP utilizando la clave privada y JSON
+    String dev = httpPetition(privateKey, json);
+    model.addAttribute("data", dev);
+
+    // Devuelve la vista correspondiente
+    return "muestra_jws"; 
+    }
+
+
+
+
+
+    
 
     private File creaFichero(String file) {
         File f = null;
@@ -96,8 +123,9 @@ public class ApplicationController {
         MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");    
         @SuppressWarnings("deprecation")
         RequestBody body = RequestBody.create(mediaType, "pem= "+ URLEncoder.encode(pem)+" &json=" + URLEncoder.encode(json));
+        logger.info("URL API: " + System.getenv("API_PROTOCOL")+"://"+System.getenv("API_HOST")+":"+System.getenv("API_PORT")+"/"+System.getenv("API_URI"));
         Request request = new Request.Builder()
-                .url("https://sergio.arlabdevelopments.com:3000/jws")
+                .url(System.getenv("API_PROTOCOL")+"://"+System.getenv("API_HOST")+":"+System.getenv("API_PORT")+"/"+System.getenv("API_URI"))
                 .method("POST", body)
                 .addHeader("Content-Type", "application/x-www-form-urlencoded")
                 .build();
@@ -105,7 +133,7 @@ public class ApplicationController {
         try {
             response = client.newCall(request).execute();
         } catch (IOException e) {
-            logger.severe("Ha habido un error en al ejecución de la llamada al API");
+            logger.severe("ha habido un error en al ejecución de la llamada al API");
             throw new RuntimeException(e);
         }
         try {
